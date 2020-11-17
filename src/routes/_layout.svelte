5720f87fe27418b33b33685227920efd8581a071
dennyfauzan <dennyfauzan92@gmail.com>
2020-11-17 10:19:41 +0700

    :art: defer css font for blocking render

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -6,10 +6,28 @@
 </script>
 
 <svelte:head>
-  <link rel="preconnect" href="https://fonts.gstatic.com" />
+  <!-- connect to domain of font files -->
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+
+  <!-- optionally increase loading priority -->
   <link
+    rel="preload"
+    as="style"
+    href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" />
+
+  <!-- async CSS -->
+  <link
+    rel="stylesheet"
     href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap"
-    rel="stylesheet" />
+    media="print"
+    onload="this.onload=null;this.removeAttribute('media');" />
+
+  <!-- no-JS fallback -->
+  <noscript>
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" />
+  </noscript>
 </svelte:head>
 
 <BaseHeader />
