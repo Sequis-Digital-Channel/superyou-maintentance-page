4843de192f838d969a3dc5e91aa87f2133b122ff
dennyfauzan <dennyfauzan92@gmail.com>
2020-11-17 17:48:57 +0700

    :lipstick: UI product benefits using dummy payload from backend

---
+++ src/components/ProductBenefits/BenefitItem.svelte
@@ -0,0 +1,66 @@
+<script>
+  export let icon;
+  export let title;
+  export let description;
+</script>
+
+<style lang="postcss">
+  .benefit_item {
+    display: flex;
+    margin-bottom: 30px;
+
+    @media (min-width: 640px) {
+      flex-direction: column;
+      align-item: center;
+      align-self: flex-start;
+      margin-bottom: 0;
+    }
+
+    &__right {
+      h4 {
+        font-size: 16px;
+        color: #0d294a;
+        font-weight: bold;
+        margin-bottom: 8px;
+      }
+      p {
+        font-size: 14px;
+        color: #708697;
+      }
+      @media (min-width: 640px) {
+        & > h4 {
+          font-size: 20px;
+          margin-bottom: 16px;
+        }
+        & > h4,
+        & > p {
+          text-align: center;
+        }
+      }
+    }
+
+    &__left {
+      flex: 1 0 50px;
+      margin-right: 16px;
+      @media (min-width: 640px) {
+        align-self: center;
+        margin-right: 0;
+        margin-bottom: 12px;
+        & > img {
+          width: 80px;
+          height: 80px;
+        }
+      }
+    }
+  }
+</style>
+
+<div class="benefit_item">
+  <div class="benefit_item__left">
+    <img src={icon} width="50" height="50" />
+  </div>
+  <div class="benefit_item__right">
+    <h4>{title.name}</h4>
+    <p>{description}</p>
+  </div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,8 @@
 <script>
   import AboveTheFold from "../components/AboveTheFold.svelte";
+  import ProductBenefits from "../components/ProductBenefits/ProductBenefits.svelte";
+  import BaseButton from "../components/BaseButton.svelte";
+  import RightArrow from "../components/svg/RightArrow.svelte";
 
   const backgroudColor = `
   linear-gradient(151deg, rgba(255, 255, 255, 0), rgba(255, 255, 255, 0.4) 100%), linear-gradient(143deg, #f7b500 22%, #e8d556 87%), linear-gradient(321deg, #fff546 95%, #e8d556 57%);
@@ -7,6 +10,15 @@
 </script>
 
 <style lang="postcss">
+  .container {
+    padding-left: 10px;
+    padding-right: 10px;
+
+    @media (min-width: 768px) {
+      padding-left: 24px;
+      padding-right: 24px;
+    }
+  }
 </style>
 
 <svelte:head>
@@ -14,3 +26,12 @@
 </svelte:head>
 
 <AboveTheFold bgGradient={backgroudColor} />
+
+<section class="container">
+  <ProductBenefits />
+
+  <BaseButton style="max-width: 294px;font-size:14px;margin:0 auto 20px;">
+    CEK MANFAAT & DETAIL PLAN
+    <RightArrow />
+  </BaseButton>
+</section>
