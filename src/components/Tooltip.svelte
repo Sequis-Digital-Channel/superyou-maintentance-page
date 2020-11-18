437a7f0003762f6ddc9f84ed363a162d994a64d0
dennyfauzan <dennyfauzan92@gmail.com>
2020-11-18 20:42:37 +0700

    :hammer: created tooltip component

---
+++ src/components/Tooltip.svelte
@@ -0,0 +1,134 @@
+<script>
+  import { createPopper } from "@popperjs/core";
+  import { onMount } from "svelte";
+  import IcTooltip from "../components/svg/IcTooltip.svelte";
+
+  export let maxWidth = "300px";
+  export let indentifier;
+
+  let popperInstance = null;
+  let button;
+  let tooltip;
+
+  let isTooltipShowing = false;
+  let toggleTooltip;
+
+  onMount(() => {
+    // button = document.querySelector("#tooltip-btn");
+    // tooltip = document.querySelector("#tooltip");
+
+    button = document.querySelector(`.${indentifier}-btn`);
+    tooltip = document.querySelector(`.${indentifier}`);
+
+    function popperCreate() {
+      popperInstance = createPopper(button, tooltip, {
+        modifiers: [
+          {
+            name: "offset",
+            options: {
+              offset: [0, 8],
+            },
+          },
+        ],
+      });
+    }
+
+    function destroy() {
+      if (popperInstance) {
+        popperInstance.destroy();
+        popperInstance = null;
+      }
+    }
+
+    toggleTooltip = function tooltipToggle() {
+      if (isTooltipShowing) {
+        tooltip.setAttribute("data-show", "false");
+        destroy();
+        isTooltipShowing = false;
+      } else {
+        tooltip.setAttribute("data-show", "true");
+        popperCreate();
+        isTooltipShowing = true;
+      }
+    };
+  });
+</script>
+
+<style lang="postcss">
+  .tooltip_wrapper {
+    display: inline-block;
+    margin-left: 6px;
+  }
+  #tooltip {
+    background-color: #03a3a6;
+    color: white;
+    padding: 12px;
+    font-size: 12px;
+    font-weight: normal;
+    border-radius: 8px;
+    text-align: left;
+    letter-spacing: 0.5px;
+    &[data-show="false"] {
+      display: none;
+    }
+    &[data-show="true"] {
+      display: block;
+    }
+  }
+  #tooltip-btn {
+    &:focus {
+      outline: none;
+    }
+  }
+
+  #arrow,
+  #arrow::before {
+    position: absolute;
+    width: 8px;
+    height: 8px;
+    z-index: -1;
+  }
+
+  #arrow::before {
+    content: "";
+    transform: rotate(45deg);
+    background: #03a3a6;
+  }
+
+  /* popper.js style */
+  :global(#tooltip[data-popper-placement^="top"] > #arrow) {
+    bottom: -4px;
+  }
+
+  :global(#tooltip[data-popper-placement^="bottom"] > #arrow) {
+    top: -4px;
+  }
+
+  :global(#tooltip[data-popper-placement^="left"] > #arrow) {
+    right: -4px;
+  }
+
+  :global(#tooltip[data-popper-placement^="right"] > #arrow) {
+    left: -4px;
+  }
+  /* end popper.js style */
+</style>
+
+<div class="tooltip_wrapper">
+  <button
+    id="tooltip-btn"
+    class={`${indentifier}-btn`}
+    aria-describedby="tooltip"
+    on:click={toggleTooltip}>
+    <IcTooltip />
+  </button>
+  <div
+    id="tooltip"
+    class={`${indentifier}`}
+    role="tooltip"
+    data-show="false"
+    style={`max-width:${maxWidth}`}>
+    <slot />
+    <div id="arrow" data-popper-arrow />
+  </div>
+</div>
