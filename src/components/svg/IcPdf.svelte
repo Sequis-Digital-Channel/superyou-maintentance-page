5b5d7234c9acf1194903ef3973f2c8b443cb23a2
dennyfauzan <dennyfauzan92@gmail.com>
2020-11-18 20:41:58 +0700

    :bento: update svg assets

---
+++ src/components/svg/IcPdf.svelte
@@ -0,0 +1,58 @@
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  xmlns:xlink="http://www.w3.org/1999/xlink"
+  width="15"
+  height="19"
+  viewBox="0 0 15 19">
+  <defs>
+    <rect id="58l88wc23a" width="295" height="40" x="0" y="0" rx="12" />
+  </defs>
+  <g fill="none" fill-rule="evenodd">
+    <g>
+      <path
+        fill="#FFF"
+        d="M0 0H375V6558H0z"
+        transform="translate(-303 -2838)" />
+      <g>
+        <g
+          transform="translate(-303 -2838) translate(41 2827) translate(262 11)">
+          <rect
+            width="13.1"
+            height="17.1"
+            x=".95"
+            y=".95"
+            stroke="#0D294A"
+            stroke-width="1.9"
+            rx="2.714" />
+          <path
+            fill="#0D294A"
+            d="M3 4.002h1.278c.627 0 1.052.42 1.052 1.064 0 .638-.447 1.055-1.096 1.055h-.492V7H3V4.002zm.742.586v.958h.336c.315 0 .501-.166.501-.478 0-.316-.184-.48-.497-.48h-.34zM5.874 4h1.225c.87 0 1.365.534 1.365 1.47C8.464 6.457 7.977 7 7.1 7H5.874V4zm.742.613v1.774h.35c.48 0 .742-.307.742-.902 0-.558-.275-.872-.742-.872h-.35zM9.756 7h-.742V4.002H11v.613H9.756v.683h1.129v.584H9.756V7z" />
+          <path
+            stroke="#0D294A"
+            stroke-linecap="square"
+            stroke-width="1.9"
+            d="M3.563 11.5L11.438 11.5M3.563 14.5L11.438 14.5" />
+        </g>
+        <g>
+          <g>
+            <g transform="translate(-303 -2838) translate(41 2827)">
+              <mask id="ov3hs3w4nb" fill="#fff">
+                <use xlink:href="#58l88wc23a" />
+              </mask>
+              <rect
+                width="294"
+                height="39"
+                x=".5"
+                y=".5"
+                stroke="#0D294A"
+                rx="12" />
+              <g stroke="#0D294A" mask="url(#ov3hs3w4nb)">
+                <path d="M0.5 0.5H294.5V39.5H0.5z" />
+              </g>
+            </g>
+          </g>
+        </g>
+      </g>
+    </g>
+  </g>
+</svg>
