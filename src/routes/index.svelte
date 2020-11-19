1457c0e8d3c9debdc9f69380d62dfdb42444c9c8
dennyfauzan <dennyfauzan92@gmail.com>
2020-11-19 09:44:19 +0700

    :art: remove unused component

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,9 +4,6 @@
   import ProductTnc from "../components/Product/ProductTnc.svelte";
   import BaseButton from "../components/BaseButton.svelte";
   import RightArrow from "../components/svg/RightArrow.svelte";
-  import Tooltip from "../components/Tooltip.svelte";
-  // import { tooltip } from "../utils/_tooltip";
-  // import IcTooltip from "../components/svg/IcTooltip.svelte";
 
   const backgroudColor = `
   linear-gradient(151deg, rgba(255, 255, 255, 0), rgba(255, 255, 255, 0.4) 100%), linear-gradient(143deg, #f7b500 22%, #e8d556 87%), linear-gradient(321deg, #fff546 95%, #e8d556 57%);
