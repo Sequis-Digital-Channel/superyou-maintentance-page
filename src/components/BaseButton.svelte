26e320c0f6cf463f00150fbc1e1c88d03fd6fd9d
dennyfauzan <dennyfauzan92@gmail.com>
2020-11-27 11:25:25 +0700

    :lipstick: button responsive padding and bgColor prop

--- src/components/BaseButton.svelte
+++ src/components/BaseButton.svelte
@@ -11,13 +11,19 @@
     display: flex;
     align-items: center;
     justify-content: space-between;
-    padding: 14px 18px;
+    padding-left: 18px;
+    padding-right: 18px;
+    height: 44px;
     border-radius: 12px;
     color: #fff;
     font-weight: bold;
     border: none;
     cursor: pointer;
     letter-spacing: 0.3px;
+
+    @media (min-width: 640px) {
+      height: 50px;
+    }
 
     &.text-center {
       text-align: center;
@@ -49,7 +55,7 @@
           d="M3.69 6.997l3.563 3.296a.948.948 0 0 1 0 1.414c-.422.39-1.107.39-1.53 0l-5.406-5a.948.948 0 0 1 0-1.414l5.407-5a1.145 1.145 0 0 1 1.53 0 .948.948 0 0 1 0 1.414l-3.559 3.29h7.224c.597 0 1.081.448 1.081 1s-.484 1-1.081 1h-7.23z" />
       </defs>
       <use
-        fill="#FFF"
+        fill={`${bgColor === 'transparent' ? '#0d294a' : '#FFF'}`}
         fill-rule="nonzero"
         transform="rotate(-180 6 6)"
         xlink:href="#a" />
