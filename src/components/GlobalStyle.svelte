882a7a39f2e88681dac0250bf7dacf714bbdb224
dennyfauzan <dennyfauzan92@gmail.com>
2020-12-01 15:06:39 +0700

    :lipstick: default style for lazyloaded element

--- src/components/GlobalStyle.svelte
+++ src/components/GlobalStyle.svelte
@@ -3,4 +3,14 @@
   :focus {
     outline: none;
   }
+  .lazy-image {
+    img {
+      opacity: 0;
+      &.lazyloaded {
+        transition: opacity 1s ease-out;
+        opacity: 1;
+        filter: none;
+      }
+    }
+  }
 </style>
