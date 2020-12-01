2bccb91e7b5809cd8381a9320003aa18671772db
dennyfauzan <dennyfauzan92@gmail.com>
2020-12-01 18:50:28 +0700

    :art: added lazy load socmed icon

--- src/components/BaseCircleSocmed.svelte
+++ src/components/BaseCircleSocmed.svelte
@@ -11,10 +11,11 @@
   }
 </style>
 
-<div class="circle-sosmed">
+<div class="circle-sosmed lazy-image">
   <slot name="icon">
     <img
-      src="/icons/socialmedia/facebook.svg"
+      src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="
+      data-src="/icons/socialmedia/facebook.svg"
       alt="social media"
       width="8"
       height="17" />
