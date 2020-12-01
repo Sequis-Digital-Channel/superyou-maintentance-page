011609081fd176be2d9291d8d62bd9e63f74d238
dennyfauzan <dennyfauzan92@gmail.com>
2020-12-01 15:26:50 +0700

    :art: put lazyLoad image code

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,7 +1,31 @@
 <script>
+  import { onMount } from "svelte";
   import GlobalStyle from "../components/GlobalStyle.svelte";
   import BaseHeader from "../components/BaseHeader.svelte";
 
+  onMount(() => {
+    const images = Array.from(document.querySelectorAll(".lazy-image img"));
+    if ("IntersectionObserver" in window) {
+      const imageObserver = new IntersectionObserver((entries, observer) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            console.log(entry.target);
+            const image = entry.target;
+            image.src = image.dataset.src;
+            image.classList.add("lazyloaded");
+            imageObserver.unobserve(image);
+          }
+        });
+      });
+
+      images.forEach((img) => imageObserver.observe(img));
+    } else {
+      console.log("asdasdasasd");
+      images.forEach((img) => {
+        img.src = img.dataset.src;
+      });
+    }
+  });
   // export let segment;
 </script>
 
