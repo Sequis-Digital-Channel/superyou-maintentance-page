f443a876978ae3c618630c05eb650c3d6489d2c3
dennyfauzan <dennyfauzan92@gmail.com>
2020-12-01 18:54:06 +0700

    :lipstick: component product cart container

---
+++ src/container/product/OtherProducts/OtherProductsContainer.svelte
@@ -0,0 +1,18 @@
+<script>
+  import BaseProductCard from "../../../components/BaseProductCard.svelte";
+</script>
+
+<style lang="postcss">
+  .other_products {
+    max-width: 1280px;
+    margin: 0 auto;
+    padding: 56px 0;
+  }
+</style>
+
+<div class="other_products">
+  <h2 class="text-xl lg:text-2xl text-center font-bold mb-6 lg:mb-10">
+    Lihat Produk Super You Lainnya
+  </h2>
+  <BaseProductCard />
+</div>
