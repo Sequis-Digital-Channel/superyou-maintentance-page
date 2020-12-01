3f86d72bcf063f34390e760f9d495c44f031f296
dennyfauzan <dennyfauzan92@gmail.com>
2020-12-01 15:05:31 +0700

    :sparkles: :zap: adding lazy load feature

--- src/client.js
+++ src/client.js
@@ -3,3 +3,23 @@ import * as sapper from '@sapper/app';
 sapper.start({
 	target: document.querySelector('#superyou')
 });
+
+  const images = Array.from(document.querySelectorAll('.lazy-image img'));
+	if ('IntersectionObserver' in window) {
+			const imageObserver = new IntersectionObserver((entries, observer) => {
+					entries.forEach(entry => {
+							if (entry.isIntersecting) {
+									const image = entry.target;
+									image.src = image.dataset.src;
+									image.classList.add("lazyloaded");
+									imageObserver.unobserve(image);
+							}
+					});
+			});
+
+			images.forEach(img => imageObserver.observe(img));
+	} else {
+		images.forEach(img => {
+			img.src = img.dataset.src;
+		});
+	}
