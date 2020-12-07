aaae9609e81c4326f548488f6f1a775adb18d5d6
dennyfauzan <dennyfauzan92@gmail.com>
2020-12-07 16:58:05 +0700

    :art: :hammer: create store for body scroll

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,8 +1,21 @@
 <script>
+  import { onMount } from "svelte";
   import GlobalStyle from "../components/GlobalStyle.svelte";
   import BaseHeader from "../components/BaseHeader.svelte";
 
+  import { bodyScroll } from "../stores/bodyscroll";
+
   export let segment;
+  let bodyElement = false;
+
+  $: if ($bodyScroll && bodyElement) {
+    document.querySelector("body").classList.remove("no-scroll");
+  } else if (!$bodyScroll && bodyElement) {
+    document.querySelector("body").classList.add("no-scroll");
+  }
+  onMount(() => {
+    bodyElement = true;
+  });
 </script>
 
 <BaseHeader {segment} />
