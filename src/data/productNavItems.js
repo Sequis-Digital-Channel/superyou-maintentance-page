b1a83aa4b0ec51c9b1ad0df4c231c02f4cf99987
dennyfauzan <dennyfauzan92@gmail.com>
2020-12-07 09:56:43 +0700

    :hammer: product nav item data

---
+++ src/data/productNavItems.js
@@ -0,0 +1,71 @@
+export const productNavItems = {
+  life: [
+    {
+      icon: "/icons/products/life/super-life.svg",
+      name: "Super Life Protection",
+      desc: "Proteksi Jiwa",
+      info: "Mulai dari Rp 33.000/Bulan",
+      product_type: "basic",
+      url: "https://superyou.co.id/produk/super-life-protection"
+    },
+    {
+      icon: "/icons/products/safe/super-safe.svg",
+      name: "Super Safe Protection",
+      desc: "Proteksi Kecelakaan",
+      info: "Mulai dari Rp 36.500/Bulan",
+      product_type: "basic",
+      url: "https://superyou.co.id/produk/super-safe-protection"
+    },
+    {
+      icon: "/icons/products/motor/super-motor.svg",
+      name: "Super Motor Protection ",
+      desc: "Proteksi Berkendara",
+      info: "Tambahan Mulai dari Rp 9.500/Bulan",
+      product_type: "rider",
+      url: "https://superyou.co.id/produk/super-safe-protection"
+    },
+    {
+      icon: "/icons/products/holiday/super-holiday.svg",
+      name: "Super Holiday Protection",
+      desc: "Proteksi Saat Liburan",
+      info: "Tambahan Mulai dari Rp 21.000/Bulan",
+      product_type: "rider",
+      url: "https://superyou.co.id/produk/super-safe-protection"
+    }
+  ],
+  health: [
+    {
+      icon: "/icons/products/hospital/my-hospital.svg",
+      name: "My Hospital Protection",
+      desc: "Santunan Rawat Inap",
+      info: "Mulai Dari Rp 39.500/Bulan",
+      product_type: "basic",
+      url: "https://superyou.co.id/produk/my-hospital-protection"
+    },
+    {
+      icon: "/icons/products/well/super-well.svg",
+      name: "Super Well Protection",
+      desc: "Perawatan 10 Penyakit Kritis",
+      info: "Mulai Dari Rp 50.000/Bulan",
+      product_type: "basic",
+      url: "/"
+    },
+    {
+      icon: "/icons/products/care/super-care.svg",
+      name: "Super Care Protection",
+      desc: "Perawatan 30 Penyakit Menular & Kecelakaan",
+      info: "Mulai Dari Rp 45.000/Bulan",
+      product_type: "basic",
+      url: "/"
+    },
+    {
+      icon: "/icons/products/strong/super-strong.svg",
+      name: "Super Strong Protection",
+      desc: "Proteksi Jiwa + 3 Penyakit Kritis",
+      info: "Mulai dari Rp 28.500/Bulan",
+      product_type: "basic",
+      url: "https://superyou.co.id/produk/super-strong-protection"
+    },
+  ]
+}
+    
