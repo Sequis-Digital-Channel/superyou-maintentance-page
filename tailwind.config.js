f21116ce635db1dc9f0067e861af523ea9f5a441
dennyfauzan <dennyfauzan92@gmail.com>
2020-12-16 19:34:52 +0700

    :wrench: added variables tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,12 +10,32 @@ module.exports = {
     fontFamily: {
       osans: ['Open Sans', '-apple-system', 'BlinkMacSystemFont']
     },
+    fontSize: {
+      ...defaultTheme.fontSize,
+      'xxs': '10px',
+      'sm2': '14px'
+    },   
     colors: {
       ...defaultTheme.colors,
-      teal: "#00aaae",
-      darkblue: "#0d294a",
-      bluegray: "#708697"
+      teal: '#00aaae',
+      darkblue: '#0d294a',
+      bluegray: '#708697'
     },
+    spacing: {
+      ...defaultTheme.spacing,
+      '2px': '2px',
+      '4px': '4px'
+    },
+    screens: {
+      ...defaultTheme.screens,
+      '374': {'max': '374px'}
+    },
+    maxWidth: {
+      ...defaultTheme.maxWidth,
+      '1/4': '25%',
+      '1/2': '50%',
+      '3/4': '75%',
+    }
   },
   plugins: [],
   future: {
