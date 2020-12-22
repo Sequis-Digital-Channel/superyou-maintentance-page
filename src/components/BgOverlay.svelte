9708811a7f7212af475cc2a7d1e81910b0a18a7a
dennyfauzan <dennyfauzan92@gmail.com>
2020-12-22 14:32:54 +0700

    :bug::lipstick: fix css props not supported by safari browser inset

--- src/components/BgOverlay.svelte
+++ src/components/BgOverlay.svelte
@@ -3,7 +3,10 @@
     background-color: rgba(0, 0, 0, 0.8);
     opacity: 0.7;
     position: fixed;
-    inset: 0px 0px 0px;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
     z-index: 9;
     will-change: opacity;
     transform: translate3d(0px, 0px, 0px);
