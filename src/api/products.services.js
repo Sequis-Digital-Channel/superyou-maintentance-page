43f6eff523abd710163c4f83094cdff31186195e
dennyfauzan <dennyfauzan92@gmail.com>
2021-01-10 15:34:34 +0700

    :truck: rename api services

---
+++ src/api/products.services.js
@@ -0,0 +1,23 @@
+export async function getPlanById (apiUrl, planId, stringDob) {
+  const url = `${apiUrl}/product_plans/${planId}/?dob=${stringDob}`;
+
+  try {
+    const res = await fetch(url);
+    const data = res.json();
+    return data;
+  } catch(err) {
+    console.log(err);
+  }
+}
+
+export async function getProductBySlugName(apiUrl, slugName, context) {
+  const url = `${apiUrl}/products/slug/${slugName}`;
+
+  try {
+    const res = await context.fetch(url);
+    const data = res.json();
+    return data;
+  } catch(err) {
+    console.log(err);
+  }
+}
