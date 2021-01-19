55b09fcdf44ce131880dbf0529c4ec42b95fb98d
dennyfauzan <dennyfauzan92@gmail.com>
2021-01-19 12:20:05 +0700

    :lipstick: :hammer: Super care form, show plan result, submit to cart

--- src/container/product/SelectPlanCare.svelte
+++ src/container/product/SelectPlanCare.svelte
@@ -14,6 +14,7 @@
   import { portal } from "../../utils/_portal";
 
   export let plans;
+  export let productSlug;
   export let api_product_url;
 
   let isAddToCartSuccess = false;
@@ -232,7 +233,11 @@
         price,
         riders: {},
       },
-      calculationData.insured_for.val.value
+      calculationData.insured_for.val.value,
+      calculationData.insured_dob.val.value,
+      basePlanResultData,
+      productSlug,
+      "SUBMIT"
     );
     isAddToCartSuccess = true;
 
@@ -254,6 +259,154 @@
   });
 </script>
 
+<div class="select-plan-care">
+  <div class="wrapper" class:show-result={planSelected}>
+    <form
+      class="premi-calculation__wrapper"
+      style="padding-bottom: 50px;"
+      on:submit|preventDefault={handleSubmittedForm}
+    >
+      <BaseSelectMenu
+        label="Pilih Plan"
+        items={productPlans}
+        bind:selectedItem={calculationData.plan.val}
+        bind:error={calculationData.plan.error}
+      />
+      <br />
+      <BaseSelectMenu
+        label="Tertanggung"
+        withtooltip={true}
+        tooltiptext="Orang yang ditanggung di dalam Polis"
+        items={insuredFor}
+        bind:selectedItem={calculationData.insured_for.val}
+        bind:error={calculationData.insured_for.error}
+      />
+      <br />
+      <BaseSelectMenu
+        label="Jenis Kelamin Tertanggung"
+        items={gender}
+        bind:selectedItem={calculationData.insured_gender.val}
+        bind:error={calculationData.insured_gender.error}
+      />
+      <br />
+      <BaseInputRadio
+        name="claim_method"
+        items={claim_method}
+        bind:selectedItemValue={calculationData.claim_method.val}
+      />
+      <br />
+      <BaseInputDate
+        name="insured_dob"
+        minAge={1}
+        maxAge={60}
+        bind:value={calculationData.insured_dob.val}
+        bind:error={calculationData.insured_dob.error}
+      />
+      <br />
+      <BaseButton style="max-width: 330px;font-size:14px;margin:30px auto 20px;"
+        >HITUNG BIAYA PREMI</BaseButton
+      >
+    </form>
+
+    <div class="plan-result">
+      {#if planSelected}
+        <div in:fly={{ y: 20, duration: 1000 }} out:fade>
+          {#if basePlanResultCard && basePlanResultData}
+            <svelte:component
+              this={basePlanResultCard}
+              plan_data={basePlanResultData}
+            />
+          {/if}
+          <BaseButton
+            on:click={(e) => handleClickAddToCart(e)}
+            style="max-width: 330px;font-size:14px;margin:30px auto 20px;"
+          >
+            TAMBAH KE KERANJANG
+            <img
+              src="/icons/basket-white.svg"
+              alt="Add to cart"
+              slot="icon"
+              width="27"
+              height="27"
+            />
+          </BaseButton>
+
+          <BaseButton
+            style="max-width: 330px;font-size:14px; color:#0d294a; border: 1px solid #0d294a;margin: 20px auto 30px;"
+            bgColor={"transparent"}
+          >BAYAR SEKARANG</BaseButton
+          >
+
+          <p
+            on:click={backToForm}
+            style="display:flex;align-items:center;text-align:center;justify-content:center;font-weight:bold;color:#0d294a;text-align:center;margin: 20px auto;font-size:14px;cursor:pointer;"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              width="12"
+              height="12"
+              viewBox="0 0 12 12">
+              <defs>
+                <path
+                  id="xopfgbdqwa"
+                  d="M7.253.293c.423.39.423 1.024 0 1.414l-3.558 3.29h7.224c.597 0 1.081.448 1.081 1s-.484 1-1.081 1h-7.23l3.564 3.296c.423.39.423 1.024 0 1.414-.422.39-1.107.39-1.53 0l-5.406-5c-.423-.39-.423-1.024 0-1.414l5.407-5c.422-.39 1.107-.39 1.53 0z"
+                />
+              </defs>
+              <g fill="none" fill-rule="evenodd">
+                <g>
+                  <g transform="translate(-20 -42) translate(20 42)">
+                    <use
+                      fill="#0D294A"
+                      fill-rule="nonzero"
+                      transform="matrix(1 0 0 -1 0 12)"
+                      xlink:href="#xopfgbdqwa"
+                    />
+                  </g>
+                </g>
+              </g>
+            </svg>
+            <span style="display:inline-block;margin-left: 14px;"
+              >LIHAT HARGA PLAN LAIN</span
+            >
+          </p>
+
+          <div style="max-width: 400px;margin: 50px auto 20px;">
+            <p style="text-align:center;color:#0d294a;font-weight:bold;">
+              SHARE
+            </p>
+
+            <div
+              class="socmed"
+              style="display:flex;justify-content:space-between;max-width:110px;margin: 14px auto;"
+            >
+              <BaseCircleSocmed />
+              <BaseCircleSocmed>
+                <img
+                  slot="icon"
+                  src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="
+                  data-src="/icons/socialmedia/twitter.svg"
+                  width="15"
+                  height="13"
+                  alt="Superyou Twitter"
+                />
+              </BaseCircleSocmed>
+            </div>
+          </div>
+        </div>
+      {/if}
+    </div>
+  </div>
+</div>
+
+<div use:portal={"#portal"}>
+  {#if isAddToCartSuccess}
+    <BgOverlay />
+    <PopUpAddToCart />
+  {/if}
+</div>
+
+<!-- <ProductRecommendationContainer /> -->
 <style lang="postcss">
   .select-plan-care {
     max-width: 400px;
@@ -282,138 +435,3 @@
     }
   }
 </style>
-
-<div class="select-plan-care">
-  <div class="wrapper" class:show-result={planSelected}>
-    <form
-      class="premi-calculation__wrapper"
-      style="padding-bottom: 50px;"
-      on:submit|preventDefault={handleSubmittedForm}>
-      <BaseSelectMenu
-        label="Pilih Plan"
-        items={productPlans}
-        bind:selectedItem={calculationData.plan.val}
-        bind:error={calculationData.plan.error} />
-      <br />
-      <BaseSelectMenu
-        label="Tertanggung"
-        withtooltip={true}
-        tooltiptext="Orang yang ditanggung di dalam Polis"
-        items={insuredFor}
-        bind:selectedItem={calculationData.insured_for.val}
-        bind:error={calculationData.insured_for.error} />
-      <br />
-      <BaseSelectMenu
-        label="Jenis Kelamin Tertanggung"
-        items={gender}
-        bind:selectedItem={calculationData.insured_gender.val}
-        bind:error={calculationData.insured_gender.error} />
-      <br />
-      <BaseInputRadio
-        name="claim_method"
-        items={claim_method}
-        bind:selectedItemValue={calculationData.claim_method.val} />
-      <br />
-      <BaseInputDate
-        name="insured_dob"
-        minAge={1}
-        maxAge={60}
-        bind:value={calculationData.insured_dob.val}
-        bind:error={calculationData.insured_dob.error} />
-      <br />
-      <BaseButton
-        style="max-width: 330px;font-size:14px;margin:30px auto 20px;">
-        HITUNG BIAYA PREMI
-      </BaseButton>
-    </form>
-
-    <div class="plan-result">
-      {#if planSelected}
-        <div in:fly={{ y: 20, duration: 1000 }} out:fade>
-          {#if basePlanResultCard && basePlanResultData}
-            <svelte:component
-              this={basePlanResultCard}
-              plan_data={basePlanResultData} />
-          {/if}
-          <BaseButton
-            on:click={(e) => handleClickAddToCart(e)}
-            style="max-width: 330px;font-size:14px;margin:30px auto 20px;">
-            TAMBAH KE KERANJANG
-            <img
-              src="/icons/basket-white.svg"
-              alt="Add to cart"
-              slot="icon"
-              width="27"
-              height="27" />
-          </BaseButton>
-
-          <BaseButton
-            style="max-width: 330px;font-size:14px; color:#0d294a; border: 1px solid #0d294a;margin: 20px auto 30px;"
-            bgColor={'transparent'}>
-            BAYAR SEKARANG
-          </BaseButton>
-
-          <p
-            on:click={backToForm}
-            style="display:flex;align-items:center;text-align:center;justify-content:center;font-weight:bold;color:#0d294a;text-align:center;margin: 20px auto;font-size:14px;cursor:pointer;">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              xmlns:xlink="http://www.w3.org/1999/xlink"
-              width="12"
-              height="12"
-              viewBox="0 0 12 12">
-              <defs>
-                <path
-                  id="xopfgbdqwa"
-                  d="M7.253.293c.423.39.423 1.024 0 1.414l-3.558 3.29h7.224c.597 0 1.081.448 1.081 1s-.484 1-1.081 1h-7.23l3.564 3.296c.423.39.423 1.024 0 1.414-.422.39-1.107.39-1.53 0l-5.406-5c-.423-.39-.423-1.024 0-1.414l5.407-5c.422-.39 1.107-.39 1.53 0z" />
-              </defs>
-              <g fill="none" fill-rule="evenodd">
-                <g>
-                  <g transform="translate(-20 -42) translate(20 42)">
-                    <use
-                      fill="#0D294A"
-                      fill-rule="nonzero"
-                      transform="matrix(1 0 0 -1 0 12)"
-                      xlink:href="#xopfgbdqwa" />
-                  </g>
-                </g>
-              </g>
-            </svg>
-            <span style="display:inline-block;margin-left: 14px;">LIHAT HARGA
-              PLAN LAIN</span>
-          </p>
-
-          <div style="max-width: 400px;margin: 50px auto 20px;">
-            <p style="text-align:center;color:#0d294a;font-weight:bold;">
-              SHARE
-            </p>
-
-            <div
-              class="socmed"
-              style="display:flex;justify-content:space-between;max-width:110px;margin: 14px auto;">
-              <BaseCircleSocmed />
-              <BaseCircleSocmed>
-                <img
-                  slot="icon"
-                  src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="
-                  data-src="/icons/socialmedia/twitter.svg"
-                  width="15"
-                  height="13"
-                  alt="Superyou Twitter" />
-              </BaseCircleSocmed>
-            </div>
-          </div>
-        </div>
-      {/if}
-    </div>
-  </div>
-</div>
-
-<div use:portal={'#portal'}>
-  {#if isAddToCartSuccess}
-    <BgOverlay />
-    <PopUpAddToCart />
-  {/if}
-</div>
-
-<!-- <ProductRecommendationContainer /> -->
