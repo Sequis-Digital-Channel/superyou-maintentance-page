f547bf4084897a1196f11a1a40af35a6e07f5c95
dennyfauzan <dennyfauzan92@gmail.com>
2021-02-16 11:07:03 +0700

    :wrench: remove lazyload icon twitter

--- src/container/product/SelectPlanCare.svelte
+++ src/container/product/SelectPlanCare.svelte
@@ -391,8 +391,7 @@
               <BaseCircleSocmed>
                 <img
                   slot="icon"
-                  src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="
-                  data-src="/icons/socialmedia/twitter.svg"
+                  src="/icons/socialmedia/twitter.svg"
                   width="15"
                   height="13"
                   alt="Superyou Twitter"
