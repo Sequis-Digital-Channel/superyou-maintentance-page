cb608b8c51c395412c4ceaf4dfd991c25d23a7e3
dennyfauzan <dennyfauzan92@gmail.com>
2021-03-01 09:29:18 +0700

    :hammer: set SelectPlan super well

--- src/routes/produk/super-well-protection.svelte
+++ src/routes/produk/super-well-protection.svelte
@@ -13,12 +13,43 @@
   import { onMount } from "svelte";
 
   import AboveTheFold from "../../container/AboveTheFold.svelte";
+  import ProductBenefits from "../../container/product/ProductBenefits.svelte";
+  import ProductTnc from "../../container/product/ProductTnc.svelte";
+  import BaseButton from "../../components/BaseButton.svelte";
   import Faq from "../../container/Faq.svelte";
 
+  import superWellProtection from "../../data/json/products/super-well/api.json";
   import heroMeta from "../../data/json/products/super-well/abovethefold-meta.json";
+  import tnc from "../../data/json/products/super-well/tnc.json";
   import faqData from "../../data/json/products/super-well/faq.json";
 
+  import { getProductBySlugNameClient } from "../../api/products.services";
+  export let api_product_url;
   export let app_url;
+
+  let plans = [];
+  let slug = superWellProtection.slug
+  let benefit_groups = superWellProtection.benefit_groups;
+  let rip_link = superWellProtection.rip_link;
+
+  let selectPlanWell;
+
+  const logError = (err) => {
+    console.error((err && err.stack) || err);
+  };
+
+  const loadSelectPlanWell = async () => {
+    const product = await getProductBySlugNameClient(
+      api_product_url,
+      "super-well-protection/?show_partner=false&is_show=true"
+    );
+    plans = product.plans;
+    import("../../container/product/SelectPlanWell.svelte")
+      .then((module) => {
+        selectPlanWell = module.default;
+      })
+      .catch(logError);
+  };
 
   onMount(() => {
     if ("IntersectionObserver" in window) {
@@ -35,6 +66,17 @@
         });
       });
       images.forEach((img) => imageObserver.observe(img));
+
+      const premiCalcContainer = document.querySelector(".premi-calculation");
+      // Form observer
+      const formObserver = new IntersectionObserver((entries) => {
+        const elForm = entries[0];
+        if (elForm.isIntersecting) {
+          loadSelectPlanWell();
+          formObserver.unobserve(premiCalcContainer);
+        }
+      });
+      formObserver.observe(premiCalcContainer);
     }
   })
   
@@ -69,6 +111,82 @@
   </AboveTheFold>
 </section>
 
+<section class="su_container benefits">
+  <ProductBenefits benefitGroups={benefit_groups}/>
+  
+  <div style="margin:0 auto 20px;max-width: 314px;">
+    <a
+      href={`${app_url}/pdf/benefits-table/tabel-manfaat-super-care-protection.pdf`}
+      target="_blank">
+      <BaseButton
+        style="font-size:14px;"
+        ariaLabel="Lihat manfaat & Detail Plan"
+      >CEK MANFAAT & DETAIL PLAN</BaseButton
+      >
+    </a>
+  </div>
+</section>
+
+<section class="su_container premi-calculation">
+  <h2 class="text-xl lg:text-2xl text-center font-bold mb-6 lg:mb-10">
+    Cari Tahu Biaya Perlindungan Super Well Protection
+  </h2>
+  {#if selectPlanWell && plans.length}
+    <svelte:component
+      this={selectPlanWell}
+      {plans}
+      {api_product_url}
+      productSlug={slug}
+    />
+  {:else}
+    <div
+      class="border border-light-gray-300 shadow rounded-md p-4 w-full mx-auto overflow-hidden"
+      style="max-width:400px"
+    >
+      <div class="animate-pulse flex flex-col space-x-4">
+        <div class="flex-1 space-y-4 py-1">
+          <div class="space-y-2">
+            <div class="h-5 bg-gray-300 rounded" />
+            <div class="h-4 bg-gray-300 rounded w-5/6" />
+            <div
+              class="h-4 bg-gray-300 rounded mb-4"
+              style="width:340px;margin-bottom: 24px;"
+            />
+            <div class="h-5 bg-gray-300 rounded" />
+            <div class="h-4 bg-gray-300 rounded w-5/6" />
+            <div
+              class="h-4 bg-gray-300 rounded mb-4"
+              style="width:340px;margin-bottom: 24px;"
+            />
+            <div class="h-5 bg-gray-300 rounded" />
+            <div class="h-4 bg-gray-300 rounded w-5/6" />
+            <div
+              class="h-4 bg-gray-300 rounded mb-4"
+              style="width:340px;margin-bottom: 24px;"
+            />
+            <div class="h-5 bg-gray-300 rounded" />
+            <div class="h-4 bg-gray-300 rounded w-5/6" />
+            <div
+              class="h-4 bg-gray-300 rounded mb-4"
+              style="width:340px;margin-bottom: 24px;"
+            />
+            <div class="h-5 bg-gray-300 rounded" />
+            <div class="h-4 bg-gray-300 rounded w-5/6" />
+            <div
+              class="h-4 bg-gray-300 rounded mb-4"
+              style="width:340px;margin-bottom: 24px;"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  {/if}
+</section>
+
+<section class="su_container tnc">
+  <ProductTnc listTnc={tnc.well} />
+</section>
+
 <section class="su_container faq">
   <Faq
     appUrl={app_url}
@@ -95,6 +213,12 @@
     @media (min-width: 768px) {
       padding-left: 24px;
       padding-right: 24px;
+    }
+
+    &.premi-calculation {
+      padding-left: 10px;
+      padding-right: 10px;
+      padding-top: 36px;
     }
   }
 </style>
