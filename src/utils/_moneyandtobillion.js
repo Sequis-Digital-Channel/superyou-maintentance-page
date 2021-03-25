e8e2093e74eaa61e2715942acd259bab7c388576
dennyfauzan <dennyfauzan92@gmail.com>
2021-03-25 10:58:11 +0700

    :hammer: utils toBillion functions

--- src/utils/_moneyandtobillion.js
+++ src/utils/_moneyandtobillion.js
@@ -1,4 +1,37 @@
+export const moneyFormat = function(value) {
+  value = "" + value;
+  return (
+    "Rp " +
+    parseFloat(value.replace(/,/g, ""))
+      .toString()
+      .replace(/\B(?=(\d{3})+(?!\d))/g, ".")
+  );
+}
+
 export const toBillion = function(value) {
+  const regExp = /[a-zA-Z]/g;
+  if (regExp.test(value)) {
+    return value;
+  }
+
+  let amountString = String(value);
+  if (amountString.length === 6) {
+    return `${amountString[0]}${amountString[1]}${amountString[2]} Ribu`;
+  } else if (amountString.length === 7) {
+    return `${amountString[0]}${amountString[1] === '0' ? '' : ',' + amountString[1]} Juta`
+  } else if (amountString.length === 8) {
+    return `${amountString[0]}${amountString[1]} Juta`;
+  } else if (amountString.length === 9) {
+    return `${amountString[0]}${amountString[1]}${amountString[2]} Juta`;
+  } else if (amountString.length === 10) {
+    return `${amountString[0]}${amountString[1] === '0' ? '' : ',' + amountString[1]} Miliar`
+  } else {
+    return amountString;
+  }
+}
+
+export const toBillionFn = function(value) {
+  // consider to not use this func since the complexity variations of amount / strings
   let moneyWording = {
     thousand: "Ribu",
     million: "Juta",
@@ -23,14 +56,4 @@ export const toBillion = function(value) {
   value = "" + value;
 
   return value;
-}
-
-export const moneyFormat = function(value) {
-  value = "" + value;
-  return (
-    "Rp " +
-    parseFloat(value.replace(/,/g, ""))
-      .toString()
-      .replace(/\B(?=(\d{3})+(?!\d))/g, ".")
-  );
 }
